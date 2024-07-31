9fe8a4fcb6b66339d15510b63642c25c292430d3
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-07-31 15:07:01 -0400

    styling the message

--- resources/views/profile/partials/sitemaps.blade.php
+++ resources/views/profile/partials/sitemaps.blade.php
@@ -69,11 +69,19 @@
                                     @endphp
                                     @if ($totalUrls > 0)
                                     {{ $processedCount }} of {{ $totalUrls }} URLs Processed
-                                    <span class="">currently processing</span>
-                                    @else
-                                    Not yet processed
-                                    @endif
+
+                                    @if ($processedCount < $totalUrls) <span class="loading-spinner-container">currently processing
+                                        <svg class="loading-spinner" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                            <path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z" />
+                                        </svg>
+                                        </span>
+                                        @endif
+
+                                        @else
+                                        Not yet processed
+                                        @endif
                                 </td>
+
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-center font-medium">
                                     <form method="POST" action="{{ route('sitemap.queue', $sitemap->id) }}">
                                         @csrf
