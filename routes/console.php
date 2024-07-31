85b0541a64ff7ad77210bda7c31e68ece6677397
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-07-31 13:07:44 -0400

    Added console route scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Jobs\ProcessQueuedUrl;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::job(new ProcessQueuedUrl)->everyMinute();
