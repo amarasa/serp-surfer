569956731b0f67d68d2e2975ffcf06ae792927b7
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-07-31 12:19:01 -0400

    Added myself as a user who can access horizon

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -29,7 +29,8 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         Gate::define('viewHorizon', function ($user) {
             return in_array($user->email, [
-                //
+                'lpangm03@me.com',
+                'angelo.marasa@hrefcreative.com'
             ]);
         });
     }
