e986ac3e5bf308776b1fb89242ab9d8f4e79dd01
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-08-03 17:58:28 -0400

    ,

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -86,6 +86,7 @@
 
 </x-app-layout>
 
+
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         const dropdownButton = document.getElementById('dropdown-button');
@@ -139,52 +140,52 @@
 
             if (urls.length === 0) {
                 tbody.innerHTML = `
-                    <tr>
-                        <td colspan="5" class="px-6 py-4 text-center">
-                            No data available for the selected domain.
-                        </td>
-                    </tr>
-                `;
+                <tr>
+                    <td colspan="5" class="px-6 py-4 text-center">
+                        No data available for the selected domain.
+                    </td>
+                </tr>
+            `;
                 return;
             }
 
             urls.forEach(url => {
                 const row = document.createElement('tr');
                 row.innerHTML = `
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <input type="checkbox" class="select-row">
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">
-                        <div>${url.page_title ?? 'No Title'}</div>
-                        <div class="text-[10px] text-gray-500 dark:text-gray-400">${url.page_url}</div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm">
-                        ${url.index_status ? 
-                            `<svg class="w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                <path fill='#157f1f' d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/>
-                            </svg>`
-                            : 
-                            `<svg class="w-6 h-6 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                <path fill='#EC0B43' d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/>
-                            </svg>`
-                        }
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm">
-                        ${url.updated_at ? timeAgo(new Date(url.updated_at)) : 'No Data'}
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm">
-                        <a href="https://google.com/search?q=site:${url.page_url}" target="_blank" class="inline-block text-blue-600 hover:text-blue-800">
-                            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-                                <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
-                            </svg>
-                        </a>
-                        <a href="${url.page_url}" target="_blank" class="inline-block ml-2 text-blue-600 hover:text-blue-800">
-                            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                <path d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6l0-128c0-17.7-14.3-32-32-32L352 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z"/>
-                            </svg>
-                        </a>
-                    </td>
-                `;
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <input type="checkbox" class="select-row">
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">
+                    <div>${url.page_title ?? 'No Title'}</div>
+                    <div class="text-[10px] text-gray-500 dark:text-gray-400">${url.page_url}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm">
+                    ${url.index_status ? 
+                        `<svg class="w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path fill='#157f1f' d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/>
+                        </svg>`
+                        : 
+                        `<svg class="w-6 h-6 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path fill='#EC0B43' d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/>
+                        </svg>`
+                    }
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm">
+                    ${url.updated_at ? timeAgo(new Date(url.updated_at)) : 'No Data'}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm">
+                    <a href="https://google.com/search?q=site:${url.page_url}" target="_blank" class="inline-block text-blue-600 hover:text-blue-800">
+                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
+                            <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+                        </svg>
+                    </a>
+                    <a href="${url.page_url}" target="_blank" class="inline-block ml-2 text-blue-600 hover:text-blue-800">
+                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6l0-128c0-17.7-14.3-32-32-32L352 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z"/>
+                        </svg>
+                    </a>
+                </td>
+            `;
                 tbody.appendChild(row);
             });
         }
@@ -231,8 +232,5 @@
             if (interval >= 1) return interval + " minute" + (interval > 1 ? "s" : "") + " ago";
             return Math.floor(seconds) + " second" + (seconds > 1 ? "s" : "") + " ago";
         }
-
-        // Initial call to bind pagination links
-        bindPaginationLinks();
     });
 </script>
