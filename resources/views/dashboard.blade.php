e64fb2860738a308c666670b51766227fd4ee9c8
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-08-03 21:26:38 -0400

    fixing pagination leaving out domains bug

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -105,11 +105,11 @@
                             @endif
                         </tbody>
                     </table>
-                    @if ($urls instanceof \Illuminate\Pagination\LengthAwarePaginator)
                     <div class="pagination-info mt-4">
-                        {{ $urls->links() }}
+                        @if ($urls instanceof \Illuminate\Pagination\LengthAwarePaginator)
+                        {{ $urls->appends(['domain' => request('domain')])->links() }}
+                        @endif
                     </div>
-                    @endif
                 </div>
             </div>
         </div>
@@ -148,23 +148,23 @@
     });
 
 
-    function timeAgo(date) {
-        const seconds = Math.floor((new Date() - date) / 1000);
-        let interval = Math.floor(seconds / 31536000);
-        if (interval >= 1)
-            return interval + " year" + (interval > 1 ? "s" : "") + " ago";
-        interval = Math.floor(seconds / 2592000);
-        if (interval >= 1)
-            return interval + " month" + (interval > 1 ? "s" : "") + " ago";
-        interval = Math.floor(seconds / 86400);
-        if (interval >= 1)
-            return interval + " day" + (interval > 1 ? "s" : "") + " ago";
-        interval = Math.floor(seconds / 3600);
-        if (interval >= 1)
-            return interval + " hour" + (interval > 1 ? "s" : "") + " ago";
-        interval = Math.floor(seconds / 60);
-        if (interval >= 1)
-            return interval + " minute" + (interval > 1 ? "s" : "") + " ago";
-        return Math.floor(seconds) + " second" + (seconds > 1 ? "s" : "") + " ago";
-    }
+    // function timeAgo(date) {
+    //     const seconds = Math.floor((new Date() - date) / 1000);
+    //     let interval = Math.floor(seconds / 31536000);
+    //     if (interval >= 1)
+    //         return interval + " year" + (interval > 1 ? "s" : "") + " ago";
+    //     interval = Math.floor(seconds / 2592000);
+    //     if (interval >= 1)
+    //         return interval + " month" + (interval > 1 ? "s" : "") + " ago";
+    //     interval = Math.floor(seconds / 86400);
+    //     if (interval >= 1)
+    //         return interval + " day" + (interval > 1 ? "s" : "") + " ago";
+    //     interval = Math.floor(seconds / 3600);
+    //     if (interval >= 1)
+    //         return interval + " hour" + (interval > 1 ? "s" : "") + " ago";
+    //     interval = Math.floor(seconds / 60);
+    //     if (interval >= 1)
+    //         return interval + " minute" + (interval > 1 ? "s" : "") + " ago";
+    //     return Math.floor(seconds) + " second" + (seconds > 1 ? "s" : "") + " ago";
+    // }
 </script>
