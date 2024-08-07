edcb0463400d6eec9176aefe38d423a616c8a175
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-08-07 14:51:31 -0400

    moving the legend

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -40,90 +40,83 @@
                 </div>
             </form>
         </div>
-    </div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                            <thead>
+                                <tr>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        <input type="checkbox" id="select-all">
+                                    </th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Page
+                                    </th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Index Status
+                                    </th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider flex items-center">
+                                        Last Seen
+                                        <svg class="ml-2 cursor-help tooltip" data-tippy-content="This was the last time that a URL was seen in the sitemap for this domain." xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16px" height="16px">
+                                            <path fill="#A0A0A0" d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3l58.3 0c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24l0-13.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1l-58.3 0c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
+                                        </svg>
+                                    </th>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                        <thead>
-                            <tr>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    <input type="checkbox" id="select-all">
-                                </th>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Page
-                                </th>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Index Status
-                                </th>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider flex items-center">
-                                    Last Seen
-                                    <svg class="ml-2 cursor-help tooltip" data-tippy-content="This was the last time that a URL was seen in the sitemap for this domain." xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16px" height="16px">
-                                        <path fill="#A0A0A0" d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3l58.3 0c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24l0-13.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1l-58.3 0c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
-                                    </svg>
-                                </th>
-
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Actions
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
-                            @if($urls->isEmpty())
-                            <tr>
-                                <td colspan="5" class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100 text-center">
-                                    Please select a domain to view the data.
-                                </td>
-                            </tr>
-                            @else
-                            @foreach($urls as $url)
-                            <tr>
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    <input type="checkbox" class="select-row">
-                                </td>
-                                <td class="px-6 py-4 text-sm font-medium text-gray-900 dark:text-gray-100">
-                                    <div class="table-cell-title tooltip-slow" data-tippy-content="{{ $url->page_title ?? 'No Title' }}">
-                                        {{ $url->page_title ?? 'No Title' }}
-                                    </div>
-                                    <div class="text-[10px] text-gray-500 dark:text-gray-400">{{ $url->page_url }}</div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm">
-                                    {!! $url->index_status ?
-                                    '<svg class="w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                        <path fill="#157f1f" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z" />
-                                    </svg>' :
-                                    '<svg class="w-6 h-6 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                        <path fill="#EC0B43" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z" />
-                                    </svg>' !!}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm">
-                                    @if ($url->urlList)
-                                    @php
-                                    $lastSeen = \Carbon\Carbon::parse($url->urlList->last_seen);
-                                    $now = \Carbon\Carbon::now();
-                                    $hoursDiff = $lastSeen ? floor($lastSeen->diffInHours($now)) : null;
-                                    $indicatorClass = '';
-
-                                    if ($hoursDiff !== null) {
-                                    if ($hoursDiff <= 24) { $indicatorClass='bg-green-500' ; } elseif ($hoursDiff <=48) { $indicatorClass='bg-yellow-500' ; } else { $indicatorClass='bg-red-500' ; } } @endphp <!-- Debugging output -->
-                                        <div style="display:none">
-                                            Last Seen: {{ $lastSeen }}
-                                            Now: {{ $now }}
-                                            Hours Difference: {{ $hoursDiff }}
+                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Actions
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
+                                @if($urls->isEmpty())
+                                <tr>
+                                    <td colspan="5" class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100 text-center">
+                                        Please select a domain to view the data.
+                                    </td>
+                                </tr>
+                                @else
+                                @foreach($urls as $url)
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <input type="checkbox" class="select-row">
+                                    </td>
+                                    <td class="px-6 py-4 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                        <div class="table-cell-title tooltip-slow" data-tippy-content="{{ $url->page_title ?? 'No Title' }}">
+                                            {{ $url->page_title ?? 'No Title' }}
                                         </div>
+                                        <div class="text-[10px] text-gray-500 dark:text-gray-400">{{ $url->page_url }}</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm">
+                                        {!! $url->index_status ?
+                                        '<svg class="w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                            <path fill="#157f1f" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z" />
+                                        </svg>' :
+                                        '<svg class="w-6 h-6 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                            <path fill="#EC0B43" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z" />
+                                        </svg>' !!}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm">
+                                        @if ($url->urlList)
+                                        @php
+                                        $lastSeen = \Carbon\Carbon::parse($url->urlList->last_seen);
+                                        $now = \Carbon\Carbon::now();
+                                        $hoursDiff = $lastSeen ? floor($lastSeen->diffInHours($now)) : null;
+                                        $indicatorClass = '';
 
-                                        @if ($hoursDiff !== null)
-                                        <span class="inline-block w-3 h-3 rounded-full blink {{ $indicatorClass }}"></span>
-                                        {{ $lastSeen->diffForHumans() }}
-                                        @else
-                                        No Data
-                                        @endif
-                                        @else
-                                        No Data
-                                        @endif
-                                </td>
+                                        if ($hoursDiff !== null) {
+                                        if ($hoursDiff <= 24) { $indicatorClass='bg-green-500' ; } elseif ($hoursDiff <=48) { $indicatorClass='bg-yellow-500' ; } else { $indicatorClass='bg-red-500' ; } } @endphp <!-- Debugging output -->
+
+                                            @if ($hoursDiff !== null)
+                                            <span class="inline-block w-3 h-3 rounded-full blink {{ $indicatorClass }}"></span>
+                                            {{ $lastSeen->diffForHumans() }}
+                                            @else
+                                            No Data
+                                            @endif
+                                            @else
+                                            No Data
+                                            @endif
+                                    </td>
 
 
 
@@ -131,48 +124,36 @@
 
 
 
-                                <td class="px-6 py-4 whitespace-nowrap text-sm">
-                                    <a href="https://google.com/search?q=site:{{ $url->page_url }}" target="_blank" class="inline-block text-blue-600 hover:text-blue-800">
-                                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-                                            <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
-                                        </svg>
-                                    </a>
-                                    <a href="{{ $url->page_url }}" target="_blank" class="inline-block ml-2 text-blue-600 hover:text-blue-800">
-                                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                            <path d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6l0-128c0-17.7-14.3-32-32-32L352 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z" />
-                                        </svg>
-                                    </a>
-                                </td>
-                            </tr>
-                            @endforeach
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm">
+                                        <a href="https://google.com/search?q=site:{{ $url->page_url }}" target="_blank" class="inline-block text-blue-600 hover:text-blue-800">
+                                            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
+                                                <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+                                            </svg>
+                                        </a>
+                                        <a href="{{ $url->page_url }}" target="_blank" class="inline-block ml-2 text-blue-600 hover:text-blue-800">
+                                            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                                <path d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6l0-128c0-17.7-14.3-32-32-32L352 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z" />
+                                            </svg>
+                                        </a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                                @endif
+                            </tbody>
+                        </table>
+                        <div class="pagination-info mt-4">
+                            @if ($urls instanceof \Illuminate\Pagination\LengthAwarePaginator)
+                            {{ $urls->appends(['domain' => request('domain')])->links() }}
                             @endif
-                        </tbody>
-                    </table>
-                    <div class="pagination-info mt-4">
-                        @if ($urls instanceof \Illuminate\Pagination\LengthAwarePaginator)
-                        {{ $urls->appends(['domain' => request('domain')])->links() }}
-                        @endif
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="mt-8">
-            <h3 class="font-semibold text-lg text-gray-800 dark:text-gray-200">Legend</h3>
-            <div class="flex items-center mt-2">
-                <span class="inline-block w-3 h-3 mr-2 rounded-full blink bg-green-500"></span>
-                <span class="text-sm text-gray-800 dark:text-gray-200">Seen within the last 24 hours</span>
-            </div>
-            <div class="flex items-center mt-2">
-                <span class="inline-block w-3 h-3 mr-2 rounded-full blink bg-yellow-500"></span>
-                <span class="text-sm text-gray-800 dark:text-gray-200">Seen within the last 24-48 hours</span>
-            </div>
-            <div class="flex items-center mt-2">
-                <span class="inline-block w-3 h-3 mr-2 rounded-full blink bg-red-500"></span>
-                <span class="text-sm text-gray-800 dark:text-gray-200">Seen more than 48 hours ago</span>
-            </div>
-        </div>
 
+        </div>
     </div>
+
+
 
 </x-app-layout>
 
