b9bc009092ac0999584bf8624acdfbc981a4e00a
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-08-07 15:11:45 -0400

    fixing a conditional issue

--- resources/views/profile/partials/sitemaps.blade.php
+++ resources/views/profile/partials/sitemaps.blade.php
@@ -80,15 +80,17 @@
                                     @endphp
                                     @if ($totalUrls > 0)
                                     {{ $processedCount }} of {{ $totalUrls }} URLs Processed
-                                    <span class="loading-spinner-container">currently processing
+                                    @if ($processedCount < $totalUrls) <span class="loading-spinner-container">currently processing
                                         <svg class="loading-spinner" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                             <path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z" />
                                         </svg>
-                                    </span>
-                                    @else
-                                    Not yet processed
-                                    @endif
+                                        </span>
+                                        @endif
+                                        @else
+                                        Not yet processed
+                                        @endif
                                 </td>
+
 
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500 dark:text-gray-300">
                                     @php
