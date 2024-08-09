fb128f53626706e5f0c45d874c64f96e34d08007
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-08-09 15:03:39 -0400

    commented out my next task

--- resources/views/admin/partials/urls-list.blade.php
+++ resources/views/admin/partials/urls-list.blade.php
@@ -1,4 +1,16 @@
 <section id="url-list">
-    {{ $ayoo }}
+    <div id="domain-selector">
+        
+        <!-- Just like the dashboard page, we'll have a drop down selector here. We select a domain and it'll make a get query call 
+         to the back end to get the URLs, just like the dashboard.
+         
+         However, the drop down select should have a search input box that will filter the domains in retail as I type
+         -->
+    </div>
+    <div id="urls-table">
+
+
+        <!-- table with URLS here, paginated to 12 -->
+    </div>
 
 </section>
