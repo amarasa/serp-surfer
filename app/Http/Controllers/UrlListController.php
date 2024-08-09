1c2d0263e4b30458e30193daa7951d6f4d75558a
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-08-09 15:29:57 -0400

    testing

--- app/Http/Controllers/UrlListController.php
+++ app/Http/Controllers/UrlListController.php
@@ -11,7 +11,7 @@ class UrlListController extends Controller
     {
         // Get distinct domains for the dropdown
         $domains = SitemapUrl::distinct()->pluck('domain_column'); // Adjust 'domain_column' to the actual column name in your table
-
+        dd($domains);
         // Get the selected domain from the request, default to the first domain if none is selected
         $selectedDomain = $request->input('domain', $domains->first());
 
