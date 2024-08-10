34ba9673289eecf11a57be9297cfb2f65b612539
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-08-09 20:02:30 -0400

    fixed typo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -82,8 +82,7 @@
                                 @foreach($urls as $url)
                                 <tr>
                                     <td class="px-6 py-4 whitespace-nowrap">
-                                        <input type="checkbox" class="select-row" name="urls[]" value="{{ $url->page_url }}" {{ $url->in_queue ? 'disabled' : '' }}>
-                                        test: {{$url->in_queue}}
+                                        <input type="checkbox" class="select-row" name="urls[]" value="{{ $url->page_url }}" {{ $url->inQueue ? 'disabled' : '' }}>
                                     </td>
                                     <td class="px-6 py-4 text-sm font-medium text-gray-900 dark:text-gray-100">
                                         <div class="table-cell-title tooltip-slow" data-tippy-content="{{ $url->page_title ?? 'No Title' }}">
@@ -97,7 +96,7 @@
                                             '<svg class="w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                                 <path fill="#157f1f" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z" />
                                             </svg>' :
-                                            ($url->in_queue ?
+                                            ($url->inQueue ?
                                             '<svg class="w-6 h-6 text-amber-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                                 <path d="M75 75L41 41C25.9 25.9 0 36.6 0 57.9L0 168c0 13.3 10.7 24 24 24l110.1 0c21.4 0 32.1-25.9 17-41l-30.8-30.8C155 85.5 203 64 256 64c106 0 192 86 192 192s-86 192-192 192c-40.8 0-78.6-12.7-109.7-34.4c-14.5-10.1-34.4-6.6-44.6 7.9s-6.6 34.4 7.9 44.6C151.2 495 201.7 512 256 512c141.4 0 256-114.6 256-256S397.4 0 256 0C185.3 0 121.3 28.7 75 75zm181 53c-13.3 0-24 10.7-24 24l0 104c0 6.4 2.5 12.5 7 17l72 72c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-65-65 0-94.1c0-13.3-10.7-24-24-24z" />
                                             </svg>' :
