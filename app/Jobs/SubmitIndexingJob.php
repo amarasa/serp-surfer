4b57d02b05332ef1415aa74a8aa6dd3e7e5a718c
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-08-12 08:53:25 -0400

    moved keys to public as a test

--- app/Jobs/SubmitIndexingJob.php
+++ app/Jobs/SubmitIndexingJob.php
@@ -70,9 +70,10 @@ class SubmitIndexingJob implements ShouldQueue
     {
         // Define the available service account keys
         $keys = [
-            '/home/forge/serpsurfer.com/resources/keys/serp-surfer-00001.json',
-            '/home/forge/serpsurfer.com/resources/keys/serp-surfer-00002.json',
+            '/home/forge/serpsurfer.com/public/keys/serp-surfer-00001.json',
+            '/home/forge/serpsurfer.com/public/keys/serp-surfer-00002.json',
         ];
+
 
 
         // Select a key (you can rotate between keys or select based on your own criteria)
