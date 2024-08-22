097a7fe1d492b3fa5d722b0f9d1835bf874b3bd3
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-08-21 20:46:45 -0400

    Footer content

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -19,8 +19,7 @@
                 </div>
 
                 <p class="mt-4 max-w-xs text-gray-500">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse non cupiditate quae nam
-                    molestias.
+                    SERP Surfer is a web application designed to help you monitor and analyze search engine results pages (SERPs) efficiently, providing valuable insights into search rankings and trends.
                 </p>
 
                 <ul class="mt-8 flex gap-6">
